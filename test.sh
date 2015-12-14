0666e4d72344a2e7c2cddee1e77d44c2e34b2d2e
Eric Romano <eromano@datto.com>
2015-12-14 18:38:21 -0500

    basic tests, less redundant code, better logging

--- test.sh
+++ test.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-#test /dank coffee
-
-curl -X POST -d @testmessage.txt http://localhost:8080 --header "Content-Type:application/json"
-echo
-
-imgur_id="676ec4b5cd4739e"
-imgur_secret="b0cfd7e695ab47ae34974408fde67d4fb5e0ebdb"
-goog_key="AIzaSyAWaPsdfQTj8zNH9nkJOe_eQV9mhccoAcA"
-python hipchat.py
+for i in ./test/* ; do
+  curl -X POST -d @$i http://localhost:8080 --header "Content-Type:application/json"
+  echo
+done
