625e3735f8553a2ad145828aa53337bcae66179a
Anthony Richards <atrichards611@gmail.com>
2016-02-24 15:43:34 -0500

    Added roll the dice feature
    Added /roll [text or number]
    Rolls randomly for positive number input
    Does not roll for 0
    Negative rolls currently broken, always return number input
    When passed words, will randomly choose one of the words

---
+++ dice.py
@@ -0,0 +1,15 @@
+# roll the dice function
+
+import random
+
+def roll_the_dice(stuff = ""):
+    if stuff.isdigit(): # If the string is all digits, will return true
+        size = int(stuff) # Because stuff is a string, we need a integer
+    else:
+        if size <= 0:
+            result = "Cannot roll 0, bro. Come on, think of a 0 sided die."
+        else:
+            result = random.randint(1,size) # Random int from 1 to size
+    else: # If not all digits, do other stuff, returns false
+        result = random.choice(stuff.split(" ")) # Uses a space as the split sperator
+    return str(result)
