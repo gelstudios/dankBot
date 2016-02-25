44d017c9fc1b826de44e942156b7a89704f28cd9
Anthony Richards <atrichards611@gmail.com>
2016-02-24 20:31:55 -0500

    Added /roll to (dev)capabilities Added empty string handling for /roll, if roll w/o arguements, you get help blurb updated /halp to include /roll added tests for /halp since none, added more /roll tests

--- dice.py
+++ dice.py
@@ -3,15 +3,18 @@
 import random
 
 def roll_the_dice(stuff = ""):
-    if stuff[0] == "-":
-        result = "There's no such thing as a negative die, bro."
+    if stuff == "":
+        result = "Bro, insert some words to get a random choice, or a number for a random number (1-#)"
     else:
-        if stuff.isdigit(): # If the string is all digits, will return true
-            size = int(stuff) # Because stuff is a string, we need a integer
-            if size <= 0:
-                result = "Cannot roll 0, bro. Come on, think of a 0 sided die."
-            else:
-                result = random.randint(1,size) # Random int from 1 to size
-        else: # If not all digits, do other stuff, returns false
-            result = random.choice(stuff.split(" ")) # Uses a space as the split sperator
+        if stuff[0] == "-":
+            result = "There's no such thing as a negative die, bro."
+        else:
+            if stuff.isdigit(): # If the string is all digits, will return true
+                size = int(stuff) # Because stuff is a string, we need a integer
+                if size <= 0:
+                    result = "Cannot roll 0, bro. Come on, think of a 0 sided die."
+                else:
+                    result = random.randint(1,size) # Random int from 1 to size
+            else: # If not all digits, do other stuff, returns false
+                result = random.choice(stuff.split(" ")) # Uses a space as the split sperator
     return str(result)
