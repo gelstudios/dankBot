b7ac805256324d6da323aeb9631138c50d65ca3c
Anthony Richards <atrichards611@gmail.com>
2016-02-25 08:43:19 -0500

    Fixed capabilities Dev now points to dev (I made a copy paste mistake) Changed string for 0 and 1 for interpolation

--- dice.py
+++ dice.py
@@ -12,7 +12,7 @@ def roll_the_dice(stuff = ""):
             if stuff.isdigit(): # If the string is all digits, will return true
                 size = int(stuff) # Because stuff is a string, we need a integer
                 if size == 0 or 1: # If the integer is 0 or 1
-                    result = "Cannot roll "+str(size)+", bro. Come on, think of a "+str(size)+" sided die."
+                    result = "Cannot roll {0}, bro. Come on, think of a {0} sided die.".format(size)
                 else:
                     result = random.randint(1,size) # Random int from 1 to size
             else: # If not all digits, do other stuff, returns false
